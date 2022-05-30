57f1d6382c0449fadedebee94af44f9f65cfca93
Amit Kumar Singh <91126008+XxFiEnDxX@users.noreply.github.com>
2022-05-30 21:53:36 +0530

    Working Javascript

--- Bunk.js
+++ Bunk.js
@@ -1,7 +1,44 @@
-var attend = document.querySelector("#AttendedB");
-var total = document.querySelector("TotalB");
+const btn = document.querySelector("#btn1");
+const output = document.querySelector("#outputTxT")
 
 let per;
-let bunk;
 
-per = (attend * 100) / total;
+
+btn.addEventListener("click", function() {
+    let bunk = 0;
+    var atn = parseInt(document.querySelector("#AttendedB").value);
+    var total = parseInt(document.querySelector("#TotalB").value);
+
+    // console.log(atn);
+    // console.log(total);
+
+    per = (atn / total) * 100;
+
+    console.log(per);
+
+    if (per >= 75) {
+        while (per >= 75) {
+            bunk = bunk + 1;
+            total = total + 1;
+            per = (atn / total) * 100;
+            // console.log(bunk)
+        };
+        bunk--;
+
+        // if (per >= 75){
+        //     cout << "You can bunk " <<  bunk << " classes" << endl;
+        //     cout << "And Your Attendence will be: " << setprecision(4) << per << " %"<< endl;
+        // }
+        // else if (per < 75){
+        //     per = (atn*100) / (total-1);
+        //     cout << "You can bunk " <<  bunk-1 << " classes" << endl;
+        //     cout << "And Your Attendence will be: " << setprecision(4) << per << " %"<< endl;
+        // }
+    };
+
+    output.innerHTML = `You can bunk ${bunk} classes.`;
+    // const Tlbunk = document.createElement('h3');
+    // Tlbunk.innerHTML = `You can bunk ${bunk} classes.`;
+    // console.log(Tlbunk);
+    // document.body.appendChild(Tlbunk)
+});
